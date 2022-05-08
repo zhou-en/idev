65a1be79e8371b1cbc29e281fa5cef3f3c1d3a37
En Zhou <zhouen.nathan@gmail.com>
2022-05-08 15:12:49 -0600

    add flags to root command

--- cmd/root.go
+++ cmd/root.go
@@ -6,9 +6,34 @@ Copyright © 2022 En Zhou <zhoeun.nathan@gmail.com>
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"net/url"
 	"os"
+	"os/exec"
 )
+
+const enUrl = "enUrl"
+const parJson = "parJson"
+const parJsonf = "parJsonf"
+
+func encodeUrl(urlStr string) {
+	fmt.Println(url.PathEscape(urlStr))
+}
+
+func parseJson(jsonStr string) {
+	cmdStr := fmt.Sprintf("echo %s | jq", jsonStr)
+	c := exec.Command("/bin/bash", "-c", cmdStr)
+	stdout, _ := c.Output()
+	fmt.Println(string(stdout))
+}
+
+func parseJsonFile(filePath string) {
+	cmdStr := fmt.Sprintf("jq 'fromjson | .' %s", filePath)
+	c := exec.Command("/bin/bash", "-c", cmdStr)
+	stdout, _ := c.Output()
+	fmt.Println(string(stdout))
+}
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -19,7 +44,32 @@ such as parse stringfied JSON to console, read a stringfied text file and parse 
 This purpose of this application is to increase the local development productivity by the simple interface.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		enUrlValue, _ := cmd.Flags().GetString(enUrl)
+		parJsonValue, _ := cmd.Flags().GetString(parJson)
+		parJsonfValue, _ := cmd.Flags().GetString(parJsonf)
+
+		switch {
+		case enUrlValue != "":
+			encodeUrl(enUrlValue)
+		case parJsonValue != "":
+			parseJson(parJsonValue)
+		case parJsonfValue != "":
+			parseJsonFile(parJsonfValue)
+		default:
+			fmt.Println("No Value was Supplied.")
+		}
+
+	},
+}
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of idev",
+	Long:  `All software has versions. This is idev's`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Hugo Static Site Generator v0.9 -- HEAD")
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -27,18 +77,29 @@ This purpose of this application is to increase the local development productivi
 func Execute() {
 	err := rootCmd.Execute()
 	if err != nil {
-		os.Exit(1)
+		os.Exit(0)
 	}
 }
 
 func init() {
+	//var Verbose bool
+
+	//var Source string
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.idev.yaml)")
-
+	//rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().StringP(enUrl, "", "", "Encode a URL")
+	rootCmd.Flags().StringP(parJson, "", "", "Parse stringfied JSON")
+	rootCmd.Flags().StringP(parJsonf, "", "", "Parse stringfied JSON from a given file")
+
+	//viper.BindPFlag("test", rootCmd.PersistentFlags().Lookup("test"))
+	//rootCmd.ParseFlags()
+	//testStr := viper.GetString("test")
+	//fmt.Println(testStr)
+	rootCmd.AddCommand(versionCmd)
 }
